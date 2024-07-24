e2e4f1fda259a43f75c58198f8116cb37cacff11
Kristin Aldridge <kristinaldridge202@gmail.com>
2024-07-23 22:49:41 -0400

    added header

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Header () {
+    return(
+        <header>
+            <h1>Satre's Lis</h1>
+            <h2>Better-Dressed People</h2>
+        </header>
+    )
+};
+
+
+export default Header
